80cf301943bf13ac5cdad730bca4125e125207b7
mbsantiago <santiago.mbal@gmail.com>
2020-05-15 14:37:34 -0500

    added list fields to terms

--- selia_admin/admin/terms.py
+++ selia_admin/admin/terms.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 
 class TermAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'term_type',
+        'value',
+    )
     search_fields = ['term_type__name', 'value']
     autocomplete_fields = ['term_type']
 
